0c24d3f811c8fda915fbd3ab7758e20f6218d46b
saihtun <saihtun1430@gmail.com>
2024-01-02 16:45:00 -0800

    modified <TimezoneSearch />

--- src/atoms/dial-colors-model.ts
+++ src/atoms/dial-colors-model.ts
@@ -1,13 +1,6 @@
 import { atom } from "jotai";
 import { DialColors } from "~/constants/colorsMap";
 
-export const dialColorsModelAtom = atom(false);
-
-export const toggleDialColorsModelAtom = atom(
-  (get) => get(dialColorsModelAtom),
-  (get, set) => set(dialColorsModelAtom, !get(dialColorsModelAtom))
-);
-
 export const dialColorAtom = atom<DialColors>("gray");
 
 export const setDialColorAtom = atom(
--- src/components/TimezoneSearch.tsx
+++ src/components/TimezoneSearch.tsx
@@ -1,18 +1,28 @@
 import { useAtom } from "jotai";
 import { searchTimezoneNameAtom } from "~/atoms/search-timezone-name";
+import { Search } from "lucide-react";
 
 export default function TimezoneSearch() {
   const [searchTimezoneName, setSearchTimezone] = useAtom(
     searchTimezoneNameAtom
   );
   return (
-    <input
-      type="text"
-      value={searchTimezoneName}
-      name="search"
-      onChange={(e) => setSearchTimezone(e.target.value)}
-      placeholder="Place or Timezone  🔍"
-      className="p-2 rounded-md text-sm pl-4 focus:outline-none primary_bg primary_border placeholder:text-xs"
-    />
+    <div className="relative top-0 right-0">
+      <input
+        type="text"
+        value={searchTimezoneName}
+        name="search"
+        onChange={(e) => setSearchTimezone(e.target.value)}
+        placeholder="Place or Timezone"
+        className="p-2 h-full w-full rounded-md text-sm pl-4 focus:outline-none primary_bg primary_border placeholder:text-xs"
+      />
+      {!searchTimezoneName && (
+        <Search
+          strokeWidth={1}
+          size={20}
+          className="absolute right-4 top-[50%] translate-y-[-50%] primary_text_gray"
+        />
+      )}
+    </div>
   );
 }
